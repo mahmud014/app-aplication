c9361c35e7dfdec73b2d596a5b6f25e3fd8e3db1
mahmud014 <anik955720@gmail.com>
2025-10-08 11:16:32 +0600

    setup projects

---
+++ src/Pages/ErrorPage/Errorpage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../Components/Header/Navbar";
+import Footer from "../../Components/Footer/Footer";
+
+const Errorpage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>404 Page not found</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Errorpage;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import Root from "../Pages/Root/Root";
+import Errorpage from "../Pages/ErrorPage/Errorpage";
+import Home from "../Pages/Home/Home";
+import Apps from "../Pages/Apps/Apps";
+import Installation from "../Pages/Installation/Installation";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Errorpage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/apps",
+        element: <Apps />,
+      },
+      {
+        path: "/installation",
+        element: <Installation />,
+      },
+    ],
+  },
+]);
